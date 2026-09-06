a45163a596a85c6fc757f63b7738c1bcb6606f31
Alex Kuznetsov <kalex7878@gmail.com>
2026-09-06 10:50:32 +0300

    init

---
+++ main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_s3_bucket" "demo" {
+  bucket = "kalex-terraform-cicd-demo-05092026"
+}
